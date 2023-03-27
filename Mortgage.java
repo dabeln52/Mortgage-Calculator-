ff07853f223b40aa6335af128c96158fbbe58149
Derek Abeln <111763839+dabeln52@users.noreply.github.com>
2023-03-27 16:18:52 -0500

    Added a method that displays a basic Payment Schedule

--- Mortgage.java
+++ Mortgage.java
@@ -1,10 +1,12 @@
+import java.text.Format;
 import java.text.NumberFormat;
 import java.util.Scanner;
 
 
 
 public class Mortgage{
-
+    final static int  month = 12;
+    final static int Ir = 100;
 public static void main(String[] args) 
 {
 // get and check Principle from the readNumber method 
@@ -17,33 +19,44 @@ double rate = readNumber("Rate: ", 0, 30);
 int year = (int)readNumber("Years: ", 1, 30);
 
 double monthlyPayment = calculateMortgage(principle, rate, year);
-NumberFormat payment = NumberFormat.getCurrencyInstance();
-String MPayment = payment.format(monthlyPayment);
-System.out.println("Your Payment " + MPayment);
+String payment = NumberFormat.getCurrencyInstance().format(monthlyPayment);
+
+System.out.println("MORTGAGE ");
+System.out.println("--------");
+System.out.println("Monthly Payments: " + payment);
+System.out.println("PAYMENT SCHEDULE");
+System.out.println("-----------------");
+for(int i = 1; i <= year * month; i++){
+    double balance = paymentSchedule(principle, rate, year, i);
+    String balanceFormat = NumberFormat.getCurrencyInstance().format(balance);
+    System.out.println(balanceFormat);
+}
+
+
+
 }
 
 public static double readNumber(String prompt, double min, double max){
-    try (Scanner scanner = new Scanner(System.in)) {
-        double value;
+     Scanner scanner = new Scanner(System.in);
+     double value;
         while(true){
             System.out.println(prompt);
-            value = scanner.nextInt();
+            value = scanner.nextDouble();
             if(value <= max || value >= min)
+            
                 break;
             System.out.println("Please enter a valid value between " + min + " and " + max);
+            scanner.close();
         }
+        
         return value;
     }
-}
+
 
 public static double calculateMortgage(
     int principle, 
     double rate,
     int year){
-        
-        final int month = 12;
-        final int Ir = 100;
-
         int numOfPayments = year * month;
         double monthlyRate = (rate/Ir) / month ;
 
@@ -51,5 +64,16 @@ public static double calculateMortgage(
 
         return monthlyPayment;
 
+}
+
+public static double paymentSchedule(int principle, double rate, int year, int numOfPayments){
+
+    int NumOfMonths = year * month;
+    double monthlyRate = (rate/Ir) / month ;
+
+double balance =  principle * (Math.pow(1 + monthlyRate, NumOfMonths) - Math.pow(1 + monthlyRate, numOfPayments)
+) / (Math.pow(1 + monthlyRate, NumOfMonths) -1);
+
+return balance;
 }
 }
